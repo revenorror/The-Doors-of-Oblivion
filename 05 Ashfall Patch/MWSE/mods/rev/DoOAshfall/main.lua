c7387aa37265f960e925aa39e6200c1e38b51089
revenorror <78823150+revenorror@users.noreply.github.com>
2021-03-20 14:42:03 -0500

    x

---
+++ 05 Ashfall Patch/MWSE/mods/rev/DoOAshfall/main.lua
@@ -0,0 +1,18 @@
+local ashfall = include("mer.ashfall.interop")
+if ashfall then
+    ashfall.registerActivators{
+		
+    }
+
+    ashfall.registerWaterContainers{
+        
+    }
+
+    ashfall.registerFoods{
+
+    }
+
+    ashfall.registerHeatSources{
+		  
+    }
+end
